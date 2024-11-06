b7decfc766bae2703fb6d0a3c11bb419c4d58d95
Fumnanya92 <104866089+Fumnanya92@users.noreply.github.com>
2024-11-06 04:50:00 +0100

    First commit

---
+++ main.tf
@@ -0,0 +1,13 @@
+module "vpc" {
+  source = "./modules/vpc"
+
+  aws_region            = var.aws_region
+  vpc_cidr              = var.vpc_cidr
+  public_subnet_1_cidr  = var.public_subnet_1_cidr
+  public_subnet_2_cidr  = var.public_subnet_2_cidr
+  private_subnet_1_cidr = var.private_subnet_1_cidr
+  private_subnet_2_cidr = var.private_subnet_2_cidr
+  availability_zone_1   = var.availability_zone_1
+  availability_zone_2   = var.availability_zone_2
+}
+
